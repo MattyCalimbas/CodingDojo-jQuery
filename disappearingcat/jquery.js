a3741f8f55f1bb994274c41667eeece16dd5b86c
Matthew Calimbas <45830182+MattyCalimbas@users.noreply.github.com>
2018-12-12 14:16:02 -0800

    First commit

---
+++ disappearingcat/jquery.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#image0').click(function(){
+        $('#ninja0').hide("slow");
+        $('#cat0').show("slow");
+    });
+    
+    $('#image1').click(function(){
+        $('#ninja1').hide("slow");
+        $('#cat1').show("slow");
+    });
+    
+    $('#image2').click(function(){
+        $('#ninja2').hide("slow");
+        $('#cat2').show("slow");
+    });
+    
+    $('#image3').click(function(){
+        $('#ninja3').hide("slow");
+        $('#cat3').show("slow");
+    });
+    
+    $('#image4').click(function(){
+        $('#ninja4').hide("slow");
+        $('#cat4').show("slow");
+    });
+});
+
